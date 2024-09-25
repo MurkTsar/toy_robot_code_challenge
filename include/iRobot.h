ab273511775eaabbd533063d1816c060f6750a79
Murk Tsar <mercoczarsalise@gmail.com>
2024-09-25 21:21:38 +0800

    TRCC-0001 create iRobot Interface

---
+++ include/iRobot.h
@@ -0,0 +1,21 @@
+#ifndef IROBOT_H
+#define IROBOT_H
+
+#include <string>
+#include "position.h"
+#include "direction.h"
+
+class iRobot {
+public:
+    virtual ~iRobot() = default;
+    
+    // Movement commands
+    virtual bool move() = 0;
+    virtual void turnLeft() = 0;
+    virtual void turnRight() = 0;
+    virtual Position reportPosition() = 0;
+    virtual Direction reportDirection() = 0;
+    virtual void pickPlace(Position p, Direction f) = 0;
+};
+
+#endif // IROBOT_H
