82c0ec7c6855f38eb4380c96ff78ae25bb76af13
Murk Tsar <mercoczarsalise@gmail.com>
2024-09-26 00:26:22 +0800

    TRCC-0003 create TableTop update erroneous syntax

--- src/tabletop.cpp
+++ src/tabletop.cpp
@@ -18,4 +18,4 @@ bool TableTop::removeRobot(std::unique_ptr<iRobot> robot) { return false; }
 
 bool TableTop::removeRobotAt(Position position) { return false; }
 
-bool TableTop::clearTableTop( return true; );
+bool TableTop::clearTableTop(){ return true; };
