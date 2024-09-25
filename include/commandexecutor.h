0e32e0c8224459c211dca06caebdb175db899e26
Murk Tsar <mercoczarsalise@gmail.com>
2024-09-26 00:31:32 +0800

    TRCC-0004 create CommandExecutor

---
+++ include/commandexecutor.h
@@ -0,0 +1,22 @@
+#ifndef COMMANDEXECUTOR_H
+#define COMMANDEXECUTOR_H
+
+#include <list>
+#include <string>
+//#include <memory>
+
+/**
+ * @class CommandExecutor
+ * @brief This is the core of the whole program
+ *
+ * This class executes the parsed commands and sets the table,
+ * robots, and the movement of the robot
+ */
+
+class CommandExecutor {
+public:
+    virtual ~CommandExecutor() = default;
+    void execute(std::list<std::string> commands);
+};
+
+#endif // COMMANDEXECUTOR_H
---
+++ src/commandexecutor.cpp
@@ -0,0 +1,6 @@
+#include "./include/commandexecutor.h"
+
+CommandExecutor::CommandExecutor() {}
+CommandExecutor::~CommandExecutor() {}
+
+void CommandExecutor::execute(std::list<std::string> commands){}
